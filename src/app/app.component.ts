e24c8633b753500760855523bbc059e7d81012de
alicemorey <alicemmorey@gmail.com>
2024-10-01 17:45:43 +0200

    user-login-component.ts install & update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -19,4 +20,10 @@ openUserRegistrationDialog(): void {
     width: '280px'
     });
   }
-}
+  openUserLoginDialog(): void {
+      this.dialog.open(UserLoginFormComponent, {
+        width: '280px'
+      });
+    }
+    
+  }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+//use this import to close the dialog on success
+import { MatDialogRef} from '@angular/material/dialog';
+//brings in the API calls created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+// used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrl: './user-login-form.component.scss'
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+    ngOnInit(): void {
+  }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+  // Logic for successful login
+     this.dialogRef.close(); // This will close the modal on success!
+     console.log(result); 
+     //store user and token in localStorage
+     localStorage.setItem('user', JSON.stringify(result.user));
+     localStorage.setItem('token', result.token);
+     this.snackBar.open('user logged in successfully!', 'OK', {
+        duration: 2000
+     });
+}, (error) => {  
+      console.log(error);   
+      this.snackBar.open(error, 'OK', {
+      duration: 2000
+     });
+    });
+}
+  }
